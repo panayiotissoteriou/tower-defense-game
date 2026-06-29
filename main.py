8180702d22cd303c47ef49bd8af0dc3a075e47b0
panayiotissoteriou <64007486+panayiotissoteriou@users.noreply.github.com>
2026-06-29 12:28:25 +0200

    make towers appear

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import enemies
-import towers
+from towers import *
 import pygame
+import towers
 
 pygame.init()
+
 
 # define screen dimensions
 WIDTH, HEIGHT = 800, 600
@@ -14,14 +16,24 @@ clock = pygame.time.Clock()
 running = True
 
 while running:
+    # create a list to store towers built and draw them in the game loop
+    towers_built = [(towers.ArrowTower(), (100, 200)), (towers.ArtilleryTower(), (300, 400)), (towers.MagicTower(), (500, 500))]
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill((20, 20, 30))  # background color
+    pygame.draw.line(screen, "gray82", (0, 0), (WIDTH, HEIGHT),  80)
 
     # draw your towers, enemies, UI here
-    pygame.draw.rect(screen, (0, 255, 0), (100, 100, 50, 50))
+    if len(towers_built) == 0:
+        towers_built = []
+    elif len(towers_built) > 0:
+        for tower, position in towers_built:
+            tower.appear(screen, tower.colour, position, 25)
+
+    # create enemies and draw them in the game loop
+    
 
     pygame.display.flip()
     clock.tick(60)
--- towers.py
+++ towers.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import pygame
 
 class Tower(ABC):
     # self.position = position
@@ -10,38 +11,52 @@ class Tower(ABC):
         self.position = [None, None]
         self.appearance = ""
         self.attack_symbol = ""
+        self.colour = ""
 
-    @abstractmethod
-    def build_position(self, ):
+    def get_build_position(self, ):
+        # TODO: return the position where the tower is built
         pass
     
-    @abstractmethod
     def attack_enemy(self, symbol):
         # TODO: if enemy within range, attack enemy with symbol
         pass
-    
+
     @abstractmethod
-    def appear(self, position):
-        pass
+    def appear(self, screen, colour, position, size=25):
+        return pygame.draw.circle(screen, colour, position, size)
 
 class ArrowTower(Tower):
     def __init__(self):
         super().__init__()
+        self.colour = "darkgoldenrod1"
     
+    def appear(self, screen, colour, position, size=25):
+        return super().appear(screen, self.colour, position, size)
+
 class ArtilleryTower(Tower):
     def __init__(self):
         super().__init__()
-        self.damage = super().damage + 10
-        self.rate = super().rate / 2
+        self.damage = self.damage + 10
+        self.rate = self.rate / 2
+        self.colour = "firebrick1"
+
+    def appear(self, screen, colour, position, size=25):
+        return super().appear(screen, self.colour, position, size)
 
 
 class MagicTower(Tower):
     def __init__(self):
         super().__init__()
-        self.damage = super().damage + 5
+        self.damage = self.damage + 5
+        self.range = self.range + 5
+        self.colour = "darkorchid"
+
+    def appear(self, screen, colour, position, size=25):
+        return super().appear(screen, self.colour, position, size)
 
 class DefenderTower(Tower):
     # builds a wall
     def __init__(self):
         super().__init__()
         self.health = 50
+        self.colour = "gray59"
